f298f6cb297c9db6157c173c689fadc48365c1fa
Thiago Mariotto <thiagomariotto@outlook.com.br>
2020-10-14 13:31:06 -0300

    config typeorm

--- server-project/src/server.ts
+++ server-project/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import './database/connection';
 
 const app = express();
 app.use(express.json());
 
 app.get('/users', (req, res) => {
     return res.json({msg: 'hello'});
-})
+});
 
 app.listen(3333);
-
