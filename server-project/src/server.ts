e42acad61f41447ba2d39d473404d4d92e58165e
Thiago Mariotto <thiagomariotto@outlook.com.br>
2020-10-14 13:00:28 -0300

    add sqlite and typeorm

--- server-project/src/server.ts
+++ server-project/src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+    return res.json({msg: 'hello'});
+})
 
 app.listen(3333);
 
